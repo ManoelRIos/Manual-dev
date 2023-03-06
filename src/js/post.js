d0c03d3d58af97703c41ad54b0351979f1905dd3
Manoel Rios <manoelriosneto@gmail.com>
2023-03-06 08:34:41 -0300

    Add modificações

--- src/js/post.js
+++ src/js/post.js
@@ -1,21 +1,23 @@
 import { posts } from "../../server/db/database.js";
 
 function addMainPost() {
-  document.querySelector(".text .title").innerHTML = posts[0].title;
-  document.querySelector(".text .description").innerHTML = posts[0].description;
-  document
-    .querySelector("#main-news .news-image #img")
-    .setAttribute("src", posts[0].image);
+  var img = document.querySelector("#main-news .news-image #img");
+  document.querySelector("#main-news .text .title").innerHTML = posts[0].title;
+  document.querySelector("#main-news .text .description").innerHTML =
+    posts[0].description;
+  img.setAttribute("src", posts[0].image);
+  img.setAttribute("alt", posts[0].title);
 }
 
 function addSecondaryPosts() {
+  var img = document.querySelector(".highlights-news .card-news #img");
   document.querySelector(".highlights-news .text h2").innerHTML =
     posts[1].title;
   document.querySelector(".highlights-news .text p").innerHTML =
     posts[1].description;
-  document
-    .querySelector(".highlights-news .card-news #img")
-    .setAttribute("src", posts[1].image);
+
+  img.setAttribute("src", posts[1].image);
+  img.setAttribute("alt", posts[1].title);
 
   var contentNews = document.querySelector(".content-news");
   for (let i = 2; i < 4; i++) {
@@ -29,5 +31,29 @@ function addSecondaryPosts() {
   }
 }
 
+function addNewsContainer() {
+  var newsContainer = document.querySelector(".news-container");
+  newsContainer.innerHtml = `<a class="news">
+  <img
+    id="img"
+    src="../public/assets/images/posts/fire-tv-stick-shutterstock_1116971222.jpg"
+    alt=""
+  />
+  <span class="date">23/02/2023</span>
+
+  <div class="text">
+    <h2 class="title">Lorem ipsum dolor, sits!</h2>
+    <p class="description">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Asperiores optio aspernatur nostrum reprehenderit, doloribus
+      exercitationem aliquam vitae fugiat eum reiciendis culpa ad
+      perferendis nobis amet, est maiores dignissimos laudantium
+      incidunt?
+    </p>
+  </div>
+</a>`;
+}
+
+addNewsContainer();
 addMainPost();
 addSecondaryPosts();
